a02688eed757c092054b01f1f854bc1059b13167
Zhonyanya <andreevich10050@gmail.com>
2025-10-19 11:37:53 +0400

    Update matrixes.py

--- matrixes.py
+++ matrixes.py
@@ -1,15 +1,17 @@
 def matrix_sum(matrix1, matrix2):
     can_sum = True
     number_of_lines = len(matrix1)
+    number_of_columns = len(matrix1[0])
     if number_of_lines == len(matrix2):
-        for column in range(number_of_lines):
-            if len(matrix1[column]) != len(matrix2[column]):
+        for line in range(number_of_lines):
+            if len(matrix1[line]) != len(matrix2[line]):
                 can_sum = False
                 break
     if can_sum:
-        new_matrix = [[0] *  len(matrix1[0])] * number_of_lines
+        new_matrix = [[0 for _ in range(number_of_columns)] for _ in range(number_of_lines)]
+        print(new_matrix)
         for line in range(number_of_lines):
-            for column in range(len(matrix1[0])):
+            for column in range(number_of_columns):
                 new_matrix[line][column] = matrix1[line][column] + matrix2[line][column]
         return new_matrix
     else:
